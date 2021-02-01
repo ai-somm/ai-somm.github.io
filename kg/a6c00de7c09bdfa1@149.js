fb5b743997ddb374698db3b95d62c592d980ae94
shenglih <shenglih@usc.edu>
2021-01-31 21:50:40 -0500

    test d3

--- kg/a6c00de7c09bdfa1@149.js
+++ kg/a6c00de7c09bdfa1@149.js
@@ -1,7 +1,7 @@
 // https://observablehq.com/@d3/force-directed-graph@149
 export default function define(runtime, observer) {
   const main = runtime.module();
-  const fileAttachments = new Map([["miserables.json",new URL("./files/31d904f6e21d42d4963ece9c8cc4fbd75efcbdc404bf511bc79906f0a1be68b5a01e935f65123670ed04e35ca8cae3c2b943f82bf8db49c5a67c85cbb58db052",import.meta.url)]]);
+  const fileAttachments = new Map([["/kg/miserables.json",new URL("./files/31d904f6e21d42d4963ece9c8cc4fbd75efcbdc404bf511bc79906f0a1be68b5a01e935f65123670ed04e35ca8cae3c2b943f82bf8db49c5a67c85cbb58db052",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], function(md){return(
 md`# Force-Directed Graph
