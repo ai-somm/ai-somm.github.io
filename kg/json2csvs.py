267a014bf7291b9de63f659be4b23412945d14fd
shenglih <shenglih@usc.edu>
2021-02-05 21:13:47 -0500

    test converts

--- kg/json2csvs.py
+++ kg/json2csvs.py
@@ -11,9 +11,48 @@ Created on Fri Feb  5 20:19:11 2021
 
 import os, json, ujson, pandas
 
-with open("/Users/sheng/somm-ai.github.io/kg/burgundy0.json","r") as js:
+##### from one json file (region.json) to two csv files (region_links.csv, region_nodes.csv)
+
+filename = "burgundy0"
+
+with open("/Users/sheng/somm-ai.github.io/kg/"+filename+".json","r") as js:
     jsfile = ujson.load(js)
     
 nodes = pandas.DataFrame({"id": [x['id'] for x in jsfile['nodes']], "group": [x['group'] for x in jsfile['nodes']]})
 
-links = pandas.DataFrame({"target": [x['target'] for x in jsfile['links']], "value": [x['value'] for x in jsfile['links']], "source": [x['source'] for x in jsfile['links']]})
+links = pandas.DataFrame({"target": [x['target'] for x in jsfile['links']], 
+                          "value": [x['value'] for x in jsfile['links']], 
+                          "source": [x['source'] for x in jsfile['links']]})
+
+nodes.to_csv("/users/sheng/somm-ai.github.io/kg/"+filename+"_nodes.csv", index=False)
+
+links.to_csv("/users/sheng/somm-ai.github.io/kg/"+filename+"_links.csv", index=False)
+
+
+##### from two csv files (region_links.csv, region_nodes.csv) to one json file (region.json) 
+from collections import defaultdict
+
+filename = "burgundy0"
+
+nodes = pandas.read_csv("/Users/sheng/somm-ai.github.io/kg/"+filename+"_nodes.csv")
+
+links = pandas.read_csv("/Users/sheng/somm-ai.github.io/kg/"+filename+"_links.csv")
+
+# check of all link ends are included in nodes, if not ERROR
+
+
+# check if all nodes' group values are filled, if not ERROR
+
+# check if all links' value fields are filled if not, auto-fill
+
+# autogenerate link values based on nodes group values
+
+jsf = defaultdict(list)
+jsf['nodes'] = [{"id":ind, "group": grp} for ind, grp in zip(nodes.id, nodes.group)]
+jsf['links'] = [{"source": src, "target": tg, "value": vl} for src, tg, vl in zip(links.source, links.target, links.value)]
+
+if os.path.exists("/Users/sheng/somm-ai.github.io/kg/"+filename+".json"):
+    print("no overeriting!!")
+else:
+    with open("/Users/sheng/somm-ai.github.io/kg/"+filename+".json","w") as tjs:
+        ujson.dump(jsf, tjs)
