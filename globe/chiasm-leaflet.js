61d14891570bcbd263e2b83ea13016f4aa2558c7
shenglih <shenglih@usc.edu>
2021-02-19 10:42:46 -0500

    alt

--- globe/chiasm-leaflet.js
+++ globe/chiasm-leaflet.js
@@ -17,8 +17,9 @@ function ChiasmLeaflet() {
 
   // Add the black & white style map layer.
   // Found by browsing http://leaflet-extras.github.io/leaflet-providers/preview/
-  L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png").addTo(map);
-  // Also try this http://{s}.tiles.earthatlas.info/natural-earth/{z}/{x}/{y}.png
+  //L.tileLayer("http://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png").addTo(map);
+  L.tileLayer("http://{s}.tiles.earthatlas.info/natural-earth/{z}/{x}/{y}.png").addTo(map);
+  // Also try this 
 
   function getCenter(){
     var center = map.getCenter();
