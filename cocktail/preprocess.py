0e6294638bd8a24211bbe3cb4029cd136fd42564
shenglih <shenglih@usc.edu>
2021-04-08 22:12:45 -0400

    grape

--- cocktail/preprocess.py
+++ cocktail/preprocess.py
@@ -39,7 +39,11 @@ ingredients = []
 with open("ingredients_processed.txt","r") as f:
     for line in f:
         ingredients.append(line.strip().split(","))
-        
+ 
+IBAcocktails = pd.read_csv("IBAcocktails0.tsv",sep="\t")
+list_of_cocktails = IBAcocktails.cocktail
+recipes = IBAcocktails.recipe
+
 from collections import defaultdict
 tail2ing = []
 for i, tail in enumerate(list_of_cocktails):
@@ -77,3 +81,76 @@ nodes.append({"name": "chartreuse", "type": "ingredient"})
 nodes1 = {}
 for node in nodes:
     nodes1[node['name']] = node
+    
+    
+########## semantic networks ##########
+ingredients0 = ingredients
+
+def replace_w_general(text):
+    if "vermouth" in text.lower():
+        text = "vermouth"
+    elif "whiskey" in text.lower() or "bourbon" in text.lower() or "scotch" in text.lower():
+        text = "whiskey"
+    elif "lemon" in text.lower():
+        text = "lemon"
+    elif "lime" in text.lower():
+        text = "lime"
+    elif "bitters" in text.lower():
+        text = "bitters"
+    elif "chartreuse" in text.lower():
+        text = "chartreuse"
+    elif "rum" in text.lower():
+        text = "rum"
+    elif "brandy" in text.lower() or "cognac" in text.lower() or "calvados" in text.lower():
+        text = "brandy"
+    elif "gin" in text.lower():
+        text = "gin"
+    elif "syrup" in text.lower():
+        text = "syrup"
+    return text
+
+# collapse lemon stuff to just lemon
+ingredients = [ [replace_w_general(ingred) for ingred in ingredient] for ingredient in ingredients]
+
+# tolower
+ingredients = [[text.lower() for text in ingre] for ingre in ingredients]
+
+# dedup
+ingredients = [list(set(ingre)) for ingre in ingredients]
+
+## single counts
+from collections import defaultdict
+ingcount1 = defaultdict(int)
+for ingredient in ingredients:
+    for ing in ingredient:
+        ingcount1[ing] += 1
+
+## combo counts
+from itertools import combinations
+ingcount2 = defaultdict(int)
+for ingredient in ingredients:
+    for pair in combinations(ingredient,2):
+        ingcount2['|'.join(list(set(pair)))] += 1
+                           
+edges = defaultdict(float)
+ingcountu = defaultdict(int)
+all_ing = set([item for sublist in ingredients for item in sublist])
+for pair in combinations(list(all_ing),2):
+    in1, in2 = pair
+    pair = '|'.join([in1,in2])
+#for pair in ingcount2:
+    #in1, in2 = pair.split("|")
+    for ingredient in ingredients:
+        if in1 in ingredient or in2 in ingredient:
+            ingcountu[pair] += 1
+    edges[pair] = ingcount2[pair]*1.0 / (ingcountu[pair])
+
+# sort by edge value
+edgesx = {k: v for k, v in sorted(edges.items(), key=lambda item: item[1], reverse=True)}
+
+
+
+
+
+
+.≥≥≥≥≤≤≤≤≤≤≤≤®
