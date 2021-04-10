34635b6e1a3d7248ccd4afa473453654f833a2c0
shenglih <shenglih@usc.edu>
2021-04-10 00:59:55 -0400

    grapes

--- kg/json2csvs.py
+++ kg/json2csvs.py
@@ -103,3 +103,81 @@ if os.path.exists("/Users/sheng/ai-somm.github.io/kg/"+filename+".json"):
 else:
     with open("/Users/sheng/ai-somm.github.io/kg/"+filename+".json","w") as tjs:
         ujson.dump(jsf, tjs)
+        
+### grapes
+
+
+filename = "grapes"
+##### from two csv files (region_links.csv, region_nodes.csv) to one json file (region.json) 
+from collections import defaultdict
+
+nodes = pandas.read_csv("/Users/sheng/Dropbox/SommIntoTheModel/grapes/graph/"+filename+"_nodes.csv")
+links = pandas.read_csv("/Users/sheng/Dropbox/SommIntoTheModel/grapes/graph/"+filename+"_links.csv")
+
+
+# check of all link ends are included in nodes, if not ERROR
+all_items = set(list(links.source)+list(links.target))
+node_pool = list(nodes.id)
+node_dict = defaultdict(int)
+for nd in node_pool:
+    node_dict[nd] += 1
+for it in all_items:
+    if node_dict[it] <= 0:
+        print("Heck, "+it+" is NOT included in nodes!!!")
+
+# check if all nodes' group values are filled, if not ERROR
+nodes[nodes.group.isna()]
+nodes[nodes.group.isnull()]
+
+# check if all links' value fields are filled if not, auto-fill
+links[links.value.isna()]
+links[links.value.isnull()]
+
+# crosswalk maps source-target-groups to link value
+# key: str(node_a.group)+"-"+str(node_b.group)
+crosswalk = defaultdict(int)
+for i in range(5):
+    crosswalk[str(i)+"-"+str(i+1)] = i
+crosswalk['5-5'] = 5
+
+#finding group value given a link source
+int(nodes.group[nodes.id==links.source[0]])
+
+# autogenerate link values based on nodes group values
+#assigning link value by node groups using crosswalk
+values = []
+for src, targ in zip(links.source[links.value.isnull()], links.target[links.value.isnull()]):
+    values.append(crosswalk[str(int(nodes.group[nodes.id==src])+"-"+str(int(nodes.group[nodes.id==targ])])
+
+links.value[links.value.isnull()] = values
+
+
+import numpy as np
+## remove nodes that are lieu-dit but not associated with any growers to make the graph look clean
+nodes_to_drop = []
+links_to_drop = []
+link_sources = set(links.source)
+for nod, gp in zip(nodes.id, nodes.group):
+    if int(gp) == 3 and nod not in link_sources:
+        print(nod)
+        #nodes_to_drop.append(np.where(nodes.id==nod))
+        nodes_to_drop.extend(np.where(np.isin(nodes.id, nod))[0].tolist())
+        #links_to_drop.append(np.where(links.target==nod))
+        links_to_drop.extend(np.where(np.isin(links.target, nod))[0].tolist())
+        
+nodes = nodes.drop(nodes_to_drop)
+links = links.drop(links_to_drop)
+
+
+### save to json file
+jsf = defaultdict(list)
+jsf['nodes'] = [{"id":ind, "group": grp} for ind, grp in zip(nodes.id, nodes.group)]
+jsf['links'] = [{"source": src, "target": tg, "value": vl} for src, tg, vl in zip(links.source, links.target, links.value)]
+
+filename = "grapes0"
+
+
+if os.path.exists("/Users/sheng/ai-somm.github.io/grape/"+filename+".json"):
+    print("no overeriting!!")
+else:
+    with open("/Users/sheng/ai-somm.github.io/grape/"+filename+".json","w") as tjs:
